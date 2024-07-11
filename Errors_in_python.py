662721c1c10416fb650c2771656677f99f3ecdbe
Nabin <nabinbhat204@gmail.com>
2024-07-11 12:24:02 +0545

    Initial Commit

---
+++ Errors_in_python.py
@@ -0,0 +1,12 @@
+num1=0.1
+num2=0.2
+num3=0.3
+print((num1+num2)==num3)
+print(num1+num2-num3)
+print(num1+num2)
+
+small = 1e-18
+print(num1+small)
+
+result = float('inf')/float('inf')
+print(result)
